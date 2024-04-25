b7918b83a5161d753b85e86044df495115e39ca0
arzamazova.nataliya <kuzelavladislav@gmail.com>
2024-04-25 09:55:41 +0300

    create new models, create html pages

--- core/app/urls.py
+++ core/app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from app.views import get_users, user_login, register
+from app.views import get_users, login, home_page, registration
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
 
+    path('', home_page, name='home_page'),
+    path('registration/', registration, name='registration'),
+    path('login/', login, name='login'),
     path('api/users/', get_users, name='get_users'),
-    path('registration',register, name='register'),
-    path('login/', user_login, name='user_login'),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/app/views.py
+++ core/app/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login
+
 
 from app.models import User
 from app.forms import SignUpForm
@@ -19,31 +21,39 @@ def get_users(request):
     return Response(user_data)
 
 
-@api_view(['POST'])
-def register(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')  # Замени 'home' на URL вашей главной страницы
-    else:
-        form = SignUpForm()
-    return render(request, 'registration/register.html', {'form': form})
+@api_view(['GET'])
+def home_page(request):
+    return render(request, 'home.html')
 
 
-@api_view(['POST'])
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            # Пользователь успешно аутентифицирован
-            return redirect('home')  # Замени 'home' на URL вашей главной страницы
-        else:
-            # Пользователь не найден или неправильный пароль
-            return render(request, 'login.html', {'error_message': 'Invalid username or password.'})
-    else:
-        return render(request, 'login.html')
+@api_view(['POST', 'GET'])
+def registration(request):
+    return render(request, 'registration.html')
+#
+# @api_view(['POST', 'GET'])
+# def register(request):
+#     if request.method == 'POST':
+#         form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('register')  # Перенаправляем на страницу регистрации
+#     else:
+#         form = SignUpForm()
+#     return render(request, 'home.html', {'form': form})
+#
+# @api_view(['POST', 'GET'])
+# def login(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             # Пользователь успешно аутентифицирован
+#             return redirect('home')  # Перенаправляем на главную страницу
+#         else:
+#             # Пользователь не найден или неправильный пароль
+#             return render(request, 'home.html', {'error_message': 'Invalid username or password.'})
+#     else:
+#         return render(request, 'home.html')
